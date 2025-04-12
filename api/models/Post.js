561e624250b0bd758ae931ccb2c286d86704366d
sbat11 <saransh.batwe518@gmail.com>
2025-04-12 18:49:45 -0400

    Removed yarn package and using it through npm

--- api/models/Post.js
+++ api/models/Post.js
@@ -13,8 +13,8 @@ const PostSchema = new mongoose.Schema({
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required:true },
     communityId: { type: mongoose.Schema.Types.ObjectId, ref: "Community", required: false }, // Optional community
     title: { type: String, required: true },
-    content: { type: String, required: true },
-    image: { type: String, required: false },
+    content: { type: String, required: true }, 
+    image: { type: String, required: false }, 
     upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     downvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }], // List of references to comments
